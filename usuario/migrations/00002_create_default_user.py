52eb5f5df6b8cd4c99be4d8727c3357153479c34
Nathalia Jacque <tata-jacque@hotmail.com>
2024-10-26 18:02:17 -0300

    correcao migration user

--- usuario/migrations/00002_create_default_user.py
+++ usuario/migrations/00002_create_default_user.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 def create_default_user(apps, schema_editor):
     User = get_user_model()
-    if not User.objects.filter(username='admin').exists():
+    if not User.objects.filter(email='admin@admin.com').exists():
         User.objects.create_superuser('admin', 'admin@admin.com', '1234')
 
 class Migration(migrations.Migration):
